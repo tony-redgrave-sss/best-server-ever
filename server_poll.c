9c2494f87e58ac1cd1366cff5174bdd3b0fdffad
tony-redgrave-sss <tonyredgrave.hunter@gmail.com>
2023-11-08 00:09:36 +0530

    select done + pls fix poll before it's too late

--- server_poll.c
+++ server_poll.c
@@ -10,9 +10,10 @@
 #include <unistd.h>
 
 #define PORT 6969
-#define MAX_CLIENTS 4000
+#define MAX_CONNECTIONS 4000
 #define BUFFER_LEN 1024
 
+// I wrote this function for what?
 int non_block(int server_fd) {
     int old_flags, new_flags;
 
@@ -32,56 +33,60 @@ int non_block(int server_fd) {
 }
 
 int main() {
-    // bunch of code to get details from server init
     struct server_result result = create_server(PORT);
-    struct sockaddr_in* server_addr = result.server_addr;
     int server_fd = result.server_fd;
-    size_t addr_len = sizeof (*(server_addr));
     printf("We are waiting for you on port %d.\n", PORT);
 
-    struct pollfd client[MAX_CLIENTS];
-    client[0].fd = server_fd;
-    client[0].events = POLLRDNORM;
-    for (int i = 1; i < MAX_CLIENTS; i++)
-        client[i].fd = -1;
+    // init pollfd struct
+    struct pollfd client_fds[MAX_CONNECTIONS];
+    client_fds[0].fd = server_fd;
+    client_fds[0].events = POLLRDNORM;
+    for (int i = 1; i < MAX_CONNECTIONS; i++) {
+        client_fds[i].fd = -1;
+    }
 
     char buffer[BUFFER_LEN];
     while (1) {
-        int nready = poll(client, MAX_CLIENTS, -1);
-        if (client[0].revents & POLLRDNORM) {
+        int next = poll(client_fds, MAX_CONNECTIONS, -1);
+        if (client_fds[0].revents & POLLRDNORM) {
             int conn_fd = accept(server_fd, (struct sockaddr*)NULL, NULL);
+            if (conn_fd < 0) {
+                perror("Accept Failed!");
+                continue;
+            }
             int i;
-            for (i = 1; i < MAX_CLIENTS; i++) {
-                if (client[i].fd < 0) {
-                    client[i].fd = conn_fd;
+            for (int i = 1; i < MAX_CONNECTIONS; i++) {
+                if (client_fds[i].fd < 0) {
+                    client_fds[i].fd = conn_fd;
                     break;
                 }
             }
-            client[i].events = POLLRDNORM;
-            if (--nready <= 0)
+            client_fds[i].events = POLLRDNORM;
+            if (--next <= 0)
                 continue;
         }
 
-        for (int i = 1; i < MAX_CLIENTS; i++) {
+        for (int i = 1; i < MAX_CONNECTIONS; i++) {
             int sockfd;
-            if ((sockfd = client[i].fd) < 0)
+            if ((sockfd = client_fds[i].fd) < 0)
                 continue;
-            if (client[i].revents & (POLLRDNORM | POLLERR)) {
+            if (client_fds[i].revents & (POLLRDNORM | POLLERR)) {
                 int n = read(sockfd, buffer, BUFFER_LEN);
                 if (n < 0) {
                     close(sockfd);
-                    client[i].fd = -1;
+                    client_fds[i].fd = -1;
                 } else if (n == 0) {
                     close(sockfd);
-                    client[i].fd = -1;
+                    client_fds[i].fd = -1;
                 } else {
                     int num = atoi(buffer);
                     unsigned long fact = factorial(num);
                     sprintf(buffer, "%lu", fact);
                     write(sockfd, buffer, strlen(buffer));
                 }
-                if (--nready <= 0)
+                if (--next <= 0) {
                     break;
+                }
             }
         }
     }
--- server_select.c
+++ server_select.c
@@ -28,61 +28,40 @@ int main() {
     fd_set set;
     char buffer[MAX_BUFFER];
     while (1) {
-        // Clear the socket set
         FD_ZERO(&set);
-
-        // Add master socket to set
         FD_SET(server_fd, &set);
         set_max = server_fd;
 
-        // Wait for an activity on one of the sockets
         activity = select(set_max + 1, &set, NULL, NULL, NULL);
-
         if ((activity < 0) && (errno != EINTR)) {
-            printf("select error");
+            fprintf(stderr, "Select Failed!\n");
         }
-
-        // If something happened on the master socket, then it's an incoming connection
+        // hello there
         if (FD_ISSET(server_fd, &set)) {
             if ((new_socket = accept(server_fd, (struct sockaddr *)&server_addr, (socklen_t*)&addr_len)) < 0) {
                 perror("accept");
                 exit(EXIT_FAILURE);
             }
-
-            printf("New connection, socket fd is %d, ip is : %s, port : %d\n", new_socket, inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
-
-            // Send new connection greeting message
-            if (send(new_socket, "Welcome to the server!\n", strlen("Welcome to the server!\n"), 0) != strlen("Welcome to the server!\n")) {
-                perror("send");
-            }
-
-            puts("Welcome message sent successfully");
-
-            // Add new socket to array of sockets
+//            printf("Hello there, %s:%d using socket fd %d.\n",
+//                   inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port), new_socket);
+            // add to fd_set
             FD_SET(new_socket, &set);
-
-            // Update the maximum file descriptor
             if (new_socket > set_max) {
                 set_max = new_socket;
             }
         }
 
-        // Check for any I/O operation on some other socket
+        // bruh
         for (i = 0; i <= set_max; i++) {
             client_socket = i;
 
             if (FD_ISSET(client_socket, &set)) {
-                // Check if it was for closing, and also read the incoming message
+                // I read nothing, so I'm assuming you have nothing to send.
                 if ((bytes_read = read(client_socket, buffer, MAX_BUFFER)) == 0) {
-                    // Somebody disconnected, get his details and print
-                    getpeername(client_socket, (struct sockaddr*)&server_addr, (socklen_t*)&addr_len);
-                    printf("Host disconnected, ip %s, port %d\n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
-
-                    // Close the socket and mark as 0 in list for reuse
                     close(client_socket);
                     FD_CLR(client_socket, &set);
                 } else {
-                    // Echo back the message that came in
+                    // Never mind, here's your factorial.
                     buffer[bytes_read] = '\0';
                     request = atoll(buffer);
                     sprintf(buffer, "%lu", factorial(request));
@@ -91,7 +70,7 @@ int main() {
             }
         }
     }
-    // Lol.
+    // Seriously. Be responsible with your return statements!
     int FRFRONG = 69;
     return FRFRONG;
 }
